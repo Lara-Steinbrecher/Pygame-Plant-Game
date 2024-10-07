0f1205dab271df7b5958505e742421b3852f29cf
JuanIgnacioTorresH <juanignaciotorr@gmail.com>
2024-10-07 16:45:10 -0300

    Agrego archivos restantes

--- menu_music.py
+++ menu_music.py
@@ -2,6 +2,40 @@ from pygame import mixer
 
 def background_music():
     mixer.init()
-    mixer.music.load('./wompwomp.mp3')
-    mixer.music.play(loops=25,fade_ms=700)
+    mixer.music.load('assets//musica//PyGameBGMusic.wav')
+    mixer.music.play(loops=-1,fade_ms=700)
     mixer.music.set_volume(0.7)
+
+def menu_music():
+    mixer.init()
+    mixer.music.load('assets//musica//MENU_SCREEN_BG.mp3')
+    mixer.music.play(loops=-1,fade_ms=700)
+    mixer.music.set_volume(0.7)
+
+def aplastar_gusano():
+    mixer.init()
+    mixer.music.load('assets//musica//aplastargusano.mp3')
+    mixer.music.play(loops=1,fade_ms=700)
+    mixer.music.set_volume(0.7)
+
+def choque_obj():
+    mixer.init()
+    mixer.music.load('assets//musica//choqueobj.mp3')
+    mixer.music.play(loops=-1,fade_ms=700)
+    mixer.music.set_volume(0.7)
+
+
+def saltito():
+    mixer.init()
+    mixer.music.load('assets//musica//saltito.mp3')
+    mixer.music.play(loops=1,fade_ms=700)
+    mixer.music.set_volume(0.7)
+
+def win_sound():
+    mixer.init()
+    mixer.music.load('assets//musica//YOUWIN.mp3')
+    mixer.music.play(loops=1,fade_ms=700)
+    mixer.music.set_volume(0.7)
+
+def stop_music():
+    mixer.music.stop()
