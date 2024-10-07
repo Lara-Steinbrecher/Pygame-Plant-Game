d4fb86fb58332c24d94d395a7f7952bea44e07c2
JuanIgnacioTorresH <juanignaciotorr@gmail.com>
2024-10-07 17:36:09 -0300

    Objeto

--- objects/needs_paper.py
+++ objects/needs_paper.py
@@ -7,8 +7,7 @@ class Needs_Paper():
         self.font = pygame.font.SysFont("Arial", 18)
         self.escale_img = escale_img
 
-        self.background = pygame.Surface((250,300))
-        self.background.fill('brown')
+        self.background = pygame.image.load("assets//images/post_it.png")
         self.background_rect = self.background.get_rect(center = (c.ANCHO // 2, c.ALTO // 2))
 
     def run(self, water, light, temp, soil):
@@ -18,13 +17,13 @@ class Needs_Paper():
         self.soil_need = soil
         self.screen.blit(self.background, self.background_rect)
 
-        self.needs_text = self.font.render("Necesidades", True, (255, 255, 255))
-        self.agua_text = self.font.render(f"Agua: {self.water_need}", True, (255, 255, 255))
-        self.luz_text = self.font.render(f"Luz: {self.light_need}", True, (255, 255, 255))
-        self.soil_text = self.font.render(f"Tierra: {self.soil_need}", True, (255, 255, 255))
-        self.temp_text = self.font.render(f"Temperatura: {self.temp_need}", True, (255, 255, 255))
+        self.needs_text = self.font.render("Necesidades", True, 'black')
+        self.agua_text = self.font.render(f"Agua: {self.water_need}", True, 'black')
+        self.luz_text = self.font.render(f"Luz: {self.light_need}", True, 'black')
+        self.soil_text = self.font.render(f"Tierra: {self.soil_need}", True, 'black')
+        self.temp_text = self.font.render(f"Temperatura: {self.temp_need}", True, 'black')
 
-        self.screen.blit(self.needs_text, (c.ANCHO // 2 - 50, c.ALTO // 2 - 120))
+        self.screen.blit(self.needs_text, (c.ANCHO // 2 - 45, c.ALTO // 2 - 180))
         self.screen.blit(self.agua_text, (c.ANCHO // 2 - 100, c.ALTO // 2 - 90))
         self.screen.blit(self.luz_text, (c.ANCHO // 2 - 100, c.ALTO // 2 - 70))
         self.screen.blit(self.soil_text, (c.ANCHO // 2 - 100, c.ALTO // 2 - 50))
