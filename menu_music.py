f1370e7a80fd4edc32e96af6a482886a603fe10f
JuanIgnacioTorresH <juanignaciotorr@gmail.com>
2024-10-07 23:35:41 -0300

    fix: fix music

--- menu_music.py
+++ menu_music.py
@@ -1,8 +1,5 @@
 from pygame import mixer
 
-channel1 = mixer.Channel(0)
-channel2 = mixer.Channel(1)
-
 def background_music():
     mixer.init()
     mixer.music.load('assets//musica//PyGameBGMusic.wav')
@@ -17,29 +14,29 @@ def menu_music():
 
 def aplastar_gusano():
     channel1 = mixer.Channel(0)
-    effect = mixer.music.load('assets//musica//aplastargusano.mp3')
+    effect = mixer.Sound('assets//musica//aplastargusano.mp3')
     channel1.play(effect)
 
 def choque_obj():
     channel1 = mixer.Channel(0)
-    effect = mixer.music.load('assets//musica//choqueobj.mp3')
+    effect = mixer.Sound('assets//musica//choqueobj.mp3')
     channel1.play(effect)
 
 
 def saltito():
     channel2 = mixer.Channel(1)
-    effect = mixer.music.load('assets//musica//saltito.mp3')
+    effect = mixer.Sound('assets//musica//saltito.mp3')
     channel2.play(effect)
 
 def win_sound():
     channel3 = mixer.Channel(2)
-    effect = mixer.music.load('assets//musica//YOUWIN.mp3')
+    effect = mixer.Sound('assets//musica//YOUWIN.mp3')
     channel3.play(effect)
     
 
 def game_over_sound():
     channel3 = mixer.Channel(2)
-    effect = mixer.music.load('assets\musica\gameoversoundeffect.mp3')
+    effect = mixer.Sound('assets//musica//gameoversoundeffect.mp3')
     channel3.play(effect)
 
 def stop_music():
