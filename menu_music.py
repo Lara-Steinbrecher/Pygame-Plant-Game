93f0dbb51c83e7c4136e4110a3a6106e1d711890
JuanIgnacioTorresH <juanignaciotorr@gmail.com>
2024-10-07 22:51:44 -0300

    fix: resuelvo problema con overlap de sonido

--- menu_music.py
+++ menu_music.py
@@ -13,32 +13,27 @@ def menu_music():
     mixer.music.set_volume(0.7)
 
 def aplastar_gusano():
-    mixer.init()
     mixer.music.load('assets//musica//aplastargusano.mp3')
     mixer.music.play(loops=1,fade_ms=700)
     mixer.music.set_volume(0.7)
 
 def choque_obj():
-    mixer.init()
     mixer.music.load('assets//musica//choqueobj.mp3')
-    mixer.music.play(loops=-1,fade_ms=700)
+    mixer.music.play(loops=1,fade_ms=700)
     mixer.music.set_volume(0.7)
 
 
 def saltito():
-    mixer.init()
     mixer.music.load('assets//musica//saltito.mp3')
     mixer.music.play(loops=1,fade_ms=700)
     mixer.music.set_volume(0.7)
 
 def win_sound():
-    mixer.init()
     mixer.music.load('assets//musica//YOUWIN.mp3')
     mixer.music.play(loops=1,fade_ms=700)
     mixer.music.set_volume(0.7)
 
 def game_over_sound():
-    mixer.init()
     mixer.music.load('assets\musica\gameoversoundeffect.mp3')
     mixer.music.play(loops=1,fade_ms=700)
     mixer.music.set_volume(0.7)
